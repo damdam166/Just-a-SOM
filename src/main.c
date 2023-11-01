d6fc399b38b993ccc1a7fe08a4e43491b1339fd8
ddelpy <damien.delpy@enseirb-matmeca.fr>
2023-11-01 21:12:30 +0100

    make a lattice

--- src/main.c
+++ src/main.c
@@ -15,6 +15,10 @@ int main(int argc, char **argv) {
     double hkjt = gaussian_n_function(1, sd);
 
     printf("%lf\n", hkjt);
-   
+  	
+    Lattice_Element *la = new_lattice(10, 10, 2, 0);
+
+    clear_lattice(la);
+
     return 0;
 }
