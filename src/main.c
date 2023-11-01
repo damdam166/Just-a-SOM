dde3afec5682a144dc7cd7f9c6d2fe2d2940257b
ddelpy <damien.delpy@enseirb-matmeca.fr>
2023-11-01 20:39:13 +0100

    make a list

--- src/main.c
+++ src/main.c
@@ -16,11 +16,5 @@ int main(int argc, char **argv) {
 
     printf("%lf\n", hkjt);
    
-    List_Element *li = new_list(1);
-
-    printf("%lf\n", li->value);
-	
-    clear_list(li);
-
     return 0;
 }
