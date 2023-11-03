ff9280f91308a83a6b73c1232f7e37f0ecd1e5c1
ddelpy <damien.delpy@enseirb-matmeca.fr>
2023-11-03 22:03:43 +0100

    update for an iteration

--- src/kohonen_algorithm/kohonen_som.c
+++ src/kohonen_algorithm/kohonen_som.c
@@ -5,16 +5,16 @@
 
 
 // Find winner
-int find_winner(int p, Lattice la, double *X) {
+int find_winner(unsigned int p, Lattice la, double *x) {
 
     if (is_empty_lattice(la)) return 0;
 
     int ind_temp = 0;
-    double norm_temp = DIST_P(p, la->dimension, la->tab_w_vectors[ind_temp].vector, X);
+    double norm_temp = DIST_P(p, la->dimension, la->tab_w_vectors[ind_temp].vector, x);
 
     for (int i_w = 0; i_w < la->lines * la->rows; ++i_w) {
 
-        if (DIST_P(p, la->dimension, la->tab_w_vectors[i_w].vector, X) <= norm_temp) ind_temp = i_w;
+        if (DIST_P(p, la->dimension, la->tab_w_vectors[i_w].vector, x) <= norm_temp) ind_temp = i_w;
     }
 
     return ind_temp;
@@ -22,7 +22,7 @@ int find_winner(int p, Lattice la, double *X) {
 
 
 // Adaptation
-void update_single_node(int dimension, double learning_rate, double standard_deviation, int p, Node pt_nd, Node pt_winner, double *X) {
+void update_single_node(unsigned int dimension, double learning_rate, double standard_deviation, unsigned int p, Node pt_nd, Node pt_winner, double *x) {
 
     if (is_empty_node(pt_nd)) return ;
 
@@ -39,12 +39,37 @@ void update_single_node(int dimension, double learning_rate, double standard_dev
     // Update by component
     for (int i_dim = 0; i_dim < dimension; ++i_dim)
 
-        pt_nd->vector[i_dim] += delta * ( X[i_dim] - pt_nd->vector[i_dim] );
+        pt_nd->vector[i_dim] += delta * (x[i_dim] - pt_nd->vector[i_dim] );
 }
 
 
-void update_for_one_iteration(Lattice la, int iteration) {
+void update_for_one_iteration(unsigned int iteration, unsigned int p, Lattice la, double *x) {
+
+    if (is_empty_lattice(la)) return ;
 
 
+    // Calculus of useful parameters
+    double learning_rate = LEARNING_RATE_FUNCTION(iteration);
+    double standard_deviation = STANDARD_DEVIATION_FUNCTION(iteration);
 
+
+    // Find the winner
+    int ind_winner = find_winner(p, la, x);
+
+
+    // Update
+        // For the winner
+    update_single_node(la->dimension, learning_rate, standard_deviation, p, &la->tab_w_vectors[ind_winner], &la->tab_w_vectors[ind_winner], x);
+
+        // For the neighbor of the left
+    if (la->tab_w_vectors[ind_winner].left_n != -1) update_single_node(la->dimension, learning_rate, standard_deviation, p, &la->tab_w_vectors[la->tab_w_vectors[ind_winner].left_n], &la->tab_w_vectors[ind_winner], x);
+
+        // For the neighbor of up
+    if (la->tab_w_vectors[ind_winner].left_n != -1) update_single_node(la->dimension, learning_rate, standard_deviation, p, &la->tab_w_vectors[la->tab_w_vectors[ind_winner].up_n], &la->tab_w_vectors[ind_winner], x);
+
+        // For the neighbor of the right
+    if (la->tab_w_vectors[ind_winner].left_n != -1) update_single_node(la->dimension, learning_rate, standard_deviation, p, &la->tab_w_vectors[la->tab_w_vectors[ind_winner].right_n], &la->tab_w_vectors[ind_winner], x);
+
+        // For the neighbor of the bottom
+    if (la->tab_w_vectors[ind_winner].left_n != -1) update_single_node(la->dimension, learning_rate, standard_deviation, p, &la->tab_w_vectors[la->tab_w_vectors[ind_winner].bottom_n], &la->tab_w_vectors[ind_winner], x);
 }
--- src/kohonen_algorithm/kohonen_som.h
+++ src/kohonen_algorithm/kohonen_som.h
@@ -5,12 +5,12 @@
 
 
     // Find winner
-    int find_winner(int p, Lattice la, double *X); // X is an input data
+    int find_winner(unsigned int p, Lattice la, double *x); // x is an input data vector
 
 
     // Adaptation
-    void update_single_node(int dimension, double learning_rate, double standard_deviation, int p, Node pt_nd, Node pt_winner, double *X);
-    void update_for_one_iteration(Lattice la, int iteration);
+    void update_single_node(unsigned int dimension, double learning_rate, double standard_deviation, unsigned int p, Node pt_nd, Node pt_winner, double *x);
+    void update_for_one_iteration(unsigned int iteration, unsigned int p, Lattice la, double *x);
 
 
 #endif
